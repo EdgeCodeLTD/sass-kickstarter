81f7db686c6aaeecc8af6275829a886dca79e520
Nikolaos Gkatzoulis <20160033@student.act.edu>
2017-04-15 20:26:05 +0300

    Updated gulpfile and added Sass partials

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,17 @@ const rename = require('gulp-rename');
 const pump = require('pump');                   // Include pump to throw useful errors
 
 gulp.task('scss', () => {                       // Create a task for gulp-sass plugin
-    return gulp.src('assets/scss/*.scss')       // Select the source to work with
+    gulp.src('assets/scss/*.scss')       // Select the source to work with
         .pipe(sass())                           // Run 'sass()' function to compile Sass code
         .pipe(gulp.dest('assets/dist/css'))     // Export to 'dist/css' folder
+});
+
+gulp.task('scss-b', () => {
+    gulp.src('assets/scss/**/*.scss')
+        .pipe(sass());
+    gulp.src('assets/scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('assets/dist/css'))
 });
 
 gulp.task('compress', function (cb) {           // Create a task for gulp-uglify plugin
@@ -23,6 +31,7 @@ gulp.task('compress', function (cb) {           // Create a task for gulp-uglify
 
 gulp.task('watch', () => {                      // Create a task to watch for changes
     gulp.watch('assets/scss/*.scss', ['scss']); // Monitor any changes made to the .scss files in specific dir
+    gulp.watch('assets/scss/**/*.scss', ['scss-b']);
     gulp.watch('assets/js/*.js', ['compress']); // Monitor any changes made to the .js files in specific dir
 });                                             // and then run 'scss' task
 
