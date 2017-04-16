348e75093c93292420c41ccfe9a052c13e3ed279
Nikolaos Gkatzoulis <20160033@student.act.edu>
2017-04-16 20:26:09 +0300

    Major changes to header intro jumbotron and navbar

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         var hash = this.hash;
 
         $('html, body').animate({
-           scrollTop: $(hash).offset().top
+           scrollTop: $(hash).offset().top - 100
         }, 800, function() {
             window.location.hash = hash;
         });
